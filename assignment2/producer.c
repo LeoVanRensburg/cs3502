5df0259a33ff30ba6d4d176b4f60798813d46af2
Leo van Rensburg <leo.van.rensburg@gmail.com>
2025-09-09 22:38:30 +0000

    successfully read from test file

--- assignment2/producer.c
+++ assignment2/producer.c
@@ -10,14 +10,17 @@ int main ( int argc , char *argv []) {
     char opt;
     int count;
     char filename[] = "test.txt";
+    int open_file = 0;
 
     // TODO : Parse command line arguments
     // -f filename ( optional )
     // -b buffer_size ( optional )
+    // Add error checking potentially?
     for( count = 0; count < argc; count++ ) {
 	// printf("CMD arguments:  argv[%d]   %s\n", count, argv[count] );	
 	if (strcmp(argv[count], "-f") == 0) {
 		strcpy(filename, argv[count + 1]);
+		open_file = 1;
 		printf("File name: %s\n", filename);
 	}
 	if (strcmp(argv[count], "-b") == 0) {
@@ -26,7 +29,22 @@ int main ( int argc , char *argv []) {
 	}
 			
     }
+
     // TODO : Open file if -f provided
+    char file_info[5000];
+    if (open_file) {
+	FILE* fptr;
+	fptr = fopen(filename, "r");
+	if (fptr == NULL) {
+		printf("The file doesn't exist or something");
+	} else {
+		while (fgets(file_info, 500, fptr) != NULL) {
+			printf("%s", file_info);
+		}
+	fclose(fptr);
+	}
+    }
+	
 
     // TODO : Allocate buffer
 

02a2683a0df657300c60cde12e8ee88f4cacfb17
Leo van Rensburg <leo.van.rensburg@gmail.com>
2025-09-09 14:19:10 +0000

    Added consumer and producer file

---
+++ assignment2/consumer.c
@@ -0,0 +1,23 @@
+# include < stdio .h >
+# include < stdlib .h >
+# include < unistd .h >
+# include < string .h >
+# include < getopt .h >
+# include < time .h >
+
+int main ( int argc , char * argv []) {
+int max_lines = -1; // -1 means unlimited
+int verbose = 0;
+
+// TODO : Parse arguments ( - n max_lines , -v verbose )
+
+// TODO : Read from stdin line by line
+// Count lines and characters
+
+// If verbose , echo lines to stdout
+
+// TODO : Print statistics to stderr
+// fprintf ( stderr , " Lines : % d \ n " , line_count ) ;
+
+return 0;
+}
---
+++ assignment2/producer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
+int main ( int argc , char *argv []) {
+    FILE *input = stdin ;
+    int buffer_size = 4096;
+    char opt;
+
+    // TODO : Parse command line arguments
+    // -f filename ( optional )
+    // -b buffer_size ( optional )
+
+    // TODO : Open file if -f provided
+
+    // TODO : Allocate buffer
+
+    // TODO : Read from input and write to stdout
+
+    // TODO : Cleanup
+    return 0;
+}

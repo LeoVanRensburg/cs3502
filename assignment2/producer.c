d8b2f6abeca392abdc855a60bd0a653ad232a4df
Leo van Rensburg <leo.van.rensburg@gmail.com>
2025-09-10 00:12:18 +0000

    Allocate buffer and refactor open file code

--- assignment2/producer.c
+++ assignment2/producer.c
@@ -9,7 +9,7 @@ int main ( int argc , char *argv []) {
     int buffer_size = 4096;
     char opt;
     int count;
-    char filename[] = "test.txt";
+    char filename[100] = "test.txt";
     int open_file = 0;
 
     // TODO : Parse command line arguments
@@ -17,36 +17,30 @@ int main ( int argc , char *argv []) {
     // -b buffer_size ( optional )
     // Add error checking potentially?
     for( count = 0; count < argc; count++ ) {
-	// printf("CMD arguments:  argv[%d]   %s\n", count, argv[count] );	
-	if (strcmp(argv[count], "-f") == 0) {
-		strcpy(filename, argv[count + 1]);
-		open_file = 1;
-		printf("File name: %s\n", filename);
-	}
-	if (strcmp(argv[count], "-b") == 0) {
-		buffer_size = atoi(argv[count + 1]);
-		printf("Buffer size: %d\n", buffer_size);
-	}
-			
+	    // printf("CMD arguments:  argv[%d]   %s\n", count, argv[count] );	
+	    if (strcmp(argv[count], "-f") == 0) {
+		    strcpy(filename, argv[count + 1]);
+		    open_file = 1;
+		    printf("File name: %s\n", filename);
+	    }
+	    if (strcmp(argv[count], "-b") == 0) {
+		    buffer_size = atoi(argv[count + 1]);
+		    printf("Buffer size: %d\n", buffer_size);
+	    }		
     }
 
     // TODO : Open file if -f provided
-    char file_info[5000];
     if (open_file) {
-	FILE* fptr;
-	fptr = fopen(filename, "r");
-	if (fptr == NULL) {
-		printf("The file doesn't exist or something");
-	} else {
-		while (fgets(file_info, 500, fptr) != NULL) {
-			printf("%s", file_info);
-		}
-	fclose(fptr);
-	}
+	    input = fopen(filename, "r");
+	    if (input == NULL) {
+		    printf("The file doesn't exist or something");
+            return 1;
+	    }
     }
-	
 
     // TODO : Allocate buffer
+    char *buffer = malloc(buffer_size);
+
 
     // TODO : Read from input and write to stdout
 

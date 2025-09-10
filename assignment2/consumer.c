8160d63a971df9358818ea544145b0ebe6671f25
Leo van Rensburg <leo.van.rensburg@gmail.com>
2025-09-10 15:43:30 +0000

    Added whole consumer logic as it was needed to test producer. Functions very similarly to producer.

--- assignment2/consumer.c
+++ assignment2/consumer.c
@@ -5,19 +5,47 @@
 #include <getopt.h>
 #include <time.h>
 
-int main ( int argc , char * argv []) {
-int max_lines = -1; // -1 means unlimited
-int verbose = 0;
+int main ( int argc , char *argv [] ) {
+    int max_lines = -1; // -1 means unlimited
+    int verbose = 0;
+    int count;
+    int line_count = 0;
+    int char_count = 0;
 
-// TODO : Parse arguments ( - n max_lines , -v verbose )
+    // Parse arguments ( - n max_lines , -v verbose )
+    for( count = 0; count < argc; count++ ) {
+        // printf("CMD arguments:  argv[%d]   %s\n", count, argv[count] );	
+        if (strcmp(argv[count], "-n") == 0) {
+            max_lines = atoi(argv[count + 1]);
+            // printf("Max Lines: %d\n", max_lines);
+        }
+        if (strcmp(argv[count], "-v") == 0) {
+            verbose = 1;
+            // printf("Verbosity: %d\n", verbose);
+        }		
+    }
 
-// TODO : Read from stdin line by line
-// Count lines and characters
+    // Reads from stdin line by line
+    // Count lines and characters
+    // If verbose , echo lines to stdout
+    char *line = NULL;
+    size_t len = 32;
+    int read;
 
-// If verbose , echo lines to stdout
+    while ((read = getline(&line, &len, stdin)) != -1) {
+        char_count += read;
+        line_count++;
+        if (verbose) {
+            printf("%s", line);
+            if (max_lines != -1 && line_count >= max_lines)
+                break;
+        }
+    }
+    fflush(stdout);     // Necessary to flush because stdout is buffered while stderr is not causing out of order printing. 
+    free(line);
 
-// TODO : Print statistics to stderr
-// fprintf ( stderr , " Lines : % d \ n " , line_count ) ;
+    // Print statistics to stderr
+    fprintf(stderr, "\nLines: %d, Characters: %d\n", line_count, char_count);
 
-return 0;
+    return 0;
 }
